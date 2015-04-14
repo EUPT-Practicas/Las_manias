f7f300d99c4363f963fab82d4910a89709d8d67f
Javi <jnavarroa90@gmail.com>
2015-04-14 12:48:49 +0200

    Mañicas
    borrado ola majo

--- LasManias/src/lasmanias/LasManias.java
+++ LasManias/src/lasmanias/LasManias.java
@@ -16,8 +16,8 @@ public class LasManias {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola majo");
         System.out.println("Menu:");
+        System.out.println("Mañicas");
     }
     
 }
