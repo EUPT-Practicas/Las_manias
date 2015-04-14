134121ef05bb77c8c345fb926ee57d2c99f39542
Javi <jnavarroa90@gmail.com>
2015-04-14 13:10:20 +0200

    Revert "Prueba"
    This reverts commit 8d754c9880444acf0d30c3471f8094bf7d7ac06b.

--- LasManias/src/lasmanias/LasManias.java
+++ LasManias/src/lasmanias/LasManias.java
@@ -16,7 +16,6 @@ public class LasManias {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola majo");
         System.out.println("Menu:");
         System.out.println("Mañicas");
     }
